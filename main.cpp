0f7c765a53adf0773626695eceb6be55b0b1c4fa
GabovYegor <gabov.egor2017@yandex.ru>
2022-07-15 17:20:03 +0300

    Implemented example for my_awaitable_type

--- main.cpp
+++ main.cpp
@@ -29,6 +29,30 @@ iteratable_generator<size_t> sequence(size_t start, size_t finish, size_t step =
         co_yield i;
 }
 
+evt_awaiter_t g_event;
+
+resumable_no_own consumer1() {
+    std::cout << "consumer1 point 1" << std::endl;
+    co_await g_event;
+    std::cout << "consumer1 point 2" << std::endl;
+    co_await g_event;
+    std::cout << "consumer1 point 3" << std::endl;
+}
+
+resumable_no_own consumer2() {
+    std::cout << "consumer2 point 1" << std::endl;
+    co_await g_event;
+    std::cout << "consumer2 point 2" << std::endl;
+    co_await g_event;
+    std::cout << "consumer2 point 3" << std::endl;
+}
+
+void producer() {
+    std::cout << "producer() is working ..." << std::endl;
+    std::cout << "Data has been prepared for consumers!" << std::endl;
+    g_event.set();
+}
+
 int main() {
     common_function();
 
@@ -45,5 +69,10 @@ int main() {
     for(auto i : sequence(10, 20, 2))
         std::cout << i << " ";
     std::cout << std::endl;
+
+    consumer1();
+    consumer2();
+    producer();
+    producer();
     return 0;
 }
